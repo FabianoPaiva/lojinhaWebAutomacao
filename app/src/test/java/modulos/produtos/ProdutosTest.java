ae84e34c9a2e71f38244ab4cd6191ad44e8cb2c9
Fabiano Paiva <fabianopaiva01@gmail.com>
2023-04-11 02:47:33 -0300

    Fazendo login e adicionando um produto

--- app/src/test/java/modulos/produtos/ProdutosTest.java
+++ app/src/test/java/modulos/produtos/ProdutosTest.java
@@ -2,6 +2,7 @@ package modulos.produtos;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,12 +22,24 @@ public class ProdutosTest {
         navegador.get("http://165.227.93.41/lojinha-web/v2/");
 
         //Fazer login
+        navegador.findElement(By.cssSelector("label[for='usuario']")).click();
+        navegador.findElement(By.id("usuario")).sendKeys("admin");
+
+        navegador.findElement(By.cssSelector("label[for='senha']")).click();
+        navegador.findElement(By.id("senha")).sendKeys("admin");
+
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
 
         //Vou para tela de registro de produto
+        navegador.findElement(By.linkText("ADICIONAR PRODUTO")).click();
 
         //Vou preencher dados do produto e o valor sera igual a zero
+        navegador.findElement(By.id("produtonome")).sendKeys("Mcbook Pro");
+        navegador.findElement(By.name("produtovalor")).sendKeys("000");
+        navegador.findElement(By.id("produtocores")).sendKeys("preto, branco");
 
         //Vou submeter o formulario
+
 
         //Vou validar que a mensagem de erro foi apresentada
     }
